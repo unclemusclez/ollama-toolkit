76e046d325063890a281dbcb9f854890d06c909b
unclemusclez <unclemusclez@gmail.com>
2024-09-21 06:42:37 -0400

    Fix submodule loop

--- dlgitdocs.sh
+++ dlgitdocs.sh
@@ -2,14 +2,16 @@
 # Array of GitHub repository URLs (replace with your actual repo URLs)
 
 repos=(
+### Insert links per line here, enclosed in ""
+## "http://github.com/example/project1.git"
+## "http://github.com/example/project2.git"
 )
+
 mkdir -p gitdocs
 cd gitdocs
 
 git init
-
-# Create a directory for submodules if it doesn't exist
-mkdir -p submodules
+git sparse-checkout set docs Docs doc DOCS Doc DOC documents Documents README.md *.md tutorials examples content guide
 
 # Loop through each repository URL
 for repo in "${repos[@]}"; do
@@ -17,11 +19,10 @@ for repo in "${repos[@]}"; do
   # Extract the repository name from the URL
   repo_name=$(basename "$repo" .git)
 
-  # Clone the repository as a submodule within "submodules/" directory
-  git clone --no-checkout --depth 1 "$repo" submodules/$repo_name
+  # Clone the repository as a submodule within the directory
+  git clone --no-checkout --depth 1 "$repo" $repo_name
 
-  cd submodules/$repo_name
-  git sparse-checkout set docs Docs doc DOCS Doc DOC documents Documents README.md *.md
+  cd $repo_name
   git checkout
 
   # Return to the parent directory
@@ -33,7 +34,7 @@ done
 # (This assumes you ran this script within your main Git project)
 
 echo "[submodule \"$repo_name\"]
-path = submodules/$repo_name
+path = ./$repo_name
 url = $repo" >> .gitmodules 
 
 # Initialize and update all submodules
